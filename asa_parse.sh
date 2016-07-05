cba6dbc1132b70502ca7e26febc6eb839e59060c
Mark Clayton <mark.clayton93@gmail.com>
2016-07-05 12:11:12 -0500

    first commit

---
+++ asa_parse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# asa_parse - A quick script to parse a Cisco ASA firewall log into the following format [date,action,source,dest]
+# Author: Mark Clayton
+# Note: The bulk of the code is in the included run.awk file. 
+
+NAME=$(echo $1 | cut -d '.' -f1)
+OUT_EXTENSION=".csv"
+OUTFILE=$NAME$OUT_EXTENSION
+echo "date,action,source,dest" > $OUTFILE
+./run.awk $1 | head -100 | uniq >> $OUTFILE 
